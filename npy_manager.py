c6821dfc5ea996a01db35660026c1528ffcc5285
zevimani <Zeviel.Imani@tufts.edu>
2023-02-28 16:24:55 -0500

    Updated npy_manager.py to include merge_samples.py

--- npy_manager.py
+++ npy_manager.py
@@ -2,15 +2,48 @@ import numpy as np
 from PIL import Image
 import os
 
-# Load in images
-inDir = "/home/zimani/"
-#inDir = "/home/zimani/score_sde_pytorch/larcv_png64_workdir/eval/ckpt_4/"
 
-#inFile = "ckpt_3_samples.npy"
-#inFile = "larcv_png_64_train_grayscale.npy"
-inFile = "gen_epoch100_orig.npy"
+## Parameters 
+genSamples = False 
 
-samples = np.load(inDir+inFile)
+ckpt = 6
+
+outfile = "gen_epoch50.npy"  
+## End Parameters 
+
+
+# Get samples from generated checkpoint (formerly merge_samples.py)  
+if genSamples: 
+
+	genDir = "/home/zimani/score_sde_pytorch/larcv_png64_workdir/eval/ckpt_"+str(ckpt)+"/"
+
+	newData = []
+	
+	# Merge all batches into single array 
+	for i in range(1,1000):
+		filename = genDir+"samples_"+str(i)+".npz" 
+		if os.path.exists(filename): 
+			sample = np.load(filename) 
+			newData.append(sample['samples'])
+		else: 
+			break
+
+	samples = np.asarray(newData) 
+
+# Load existing numpy file 
+else:	
+
+	inDir = "/home/zimani/"
+
+	#inFile = "ckpt_3_samples.npy"
+	#inFile = "larcv_png_64_train_grayscale.npy"
+	inFile = "gen_epoch300_orig.npy"
+	
+	# Remove last underscore from name 
+	outFile = "_".join(inFile.split("_")[:-1])+".npy"
+
+	samples = np.load(inDir+inFile)
+
 
 # Optional: trim number of samples to 32 
 #trimmed = samples[0:32,:,:] 
@@ -29,11 +62,8 @@ batches = int(maxSamples/bSize) + 1 #rounding
 # Convert to grayscale npy files
 saveGray = True
 grayImages = []
-#outFile = inFile.rsplit(".", 1)[0] + "_grayscale.npy"
-outFile = "_".join(inFile.split("_")[:-1])+".npy" #remove last underscore 
-#outfile = "gen_epoch50.npy"  
 
-# Save a sample png 
+# Option: save a sample png 
 saveSamples = False
 sampleDir = ""
 
@@ -74,6 +104,5 @@ for j in range(0, batches):
 if saveGray: 
 	np.save(outFile, grayImages) 
 
-#print(np.asarray(grayImages).shape) 
-print(totSamples, "Images Grayscaled")     
+print(totSamples, "Images Processed")     
 

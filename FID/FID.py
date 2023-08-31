f4bea11d08b6e14b68c8faf175e7860a1abc7832
zevimani <Zeviel.Imani@tufts.edu>
2023-08-31 17:30:32 -0400

    Finalized SSNet-FID Scripts

--- FID/FID.py
+++ FID/FID.py
@@ -1,57 +1,71 @@
 import numpy as np
-from tqdm import tqdm 
-import torch
 from scipy import linalg
 
+#import torch
 #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-larcv = np.load("larcv_png_64_train_FID.npy") 
-#larcv = np.load("larcv_png_64_test_FID.npy") 
-larcv = larcv.reshape(larcv.shape[0], -1) 
+larcvT = np.load("./ssnet_activations/larcv_png_64_train_FID.npy") 
+larcvT = larcvT.reshape(larcvT.shape[0], -1) 
+larcvV = np.load("./ssnet_activations/larcv_png_64_test_FID.npy") 
+larcvV = larcvV.reshape(larcvV.shape[0], -1) 
 
-# Downsample larcv train to match larcv val 
-if 1: 
-	n = 10000 
-	idxLar = np.random.choice(np.arange(larcv.shape[0]), size=n, replace=False) 
-	larcv = larcv[idxLar] 
+# Downsample training larcv dataset to larcv dataset size   
+if larcvT.shape[0] > larcvV.shape[0]: 
+	idxLar = np.random.choice(np.arange(larcvT.shape[0]), size=larcvV.shape[0], replace=False) 
+	larcvT = larcvT[idxLar] 
 
-sigma1 = np.cov(larcv, rowvar=False) 
-mu1 = larcv.mean(axis=0) 
+sigmaT = np.cov(larcvT, rowvar=False) 
+muT = larcvT.mean(axis=0) 
+sigmaV = np.cov(larcvV, rowvar=False) 
+muV = larcvV.mean(axis=0) 
 
-fileNames = ['gen_epoch10', "gen_epoch20", "gen_epoch30", 
+fileNames = ["gen_epoch10", "gen_epoch20", "gen_epoch30", 
 	"gen_epoch40", "gen_epoch50", "gen_epoch60", 
 	"gen_epoch100", "gen_epoch150", "gen_epoch300", "VQVAE"] 
 
+FIDs = [] 
 
-for fileName in fileNames:
+print("FileName, Training FID, Validation FID") 
+
+for i, fileName in enumerate(fileNames):
 
 	gen = np.load("./ssnet_activations/"+fileName+"_FID.npy") 
 	gen = gen.reshape(gen.shape[0], -1) 
 	
 	# Downsample generated images to larcv comparison size 
-	if larcv.shape[0] != gen.shape[0]: 
-		idx = np.random.choice(np.arange(gen.shape[0]), size=larcv.shape[0], replace=False)  
+	if larcvV.shape[0] != gen.shape[0]: 
+		idx = np.random.choice(np.arange(gen.shape[0]), size=larcvV.shape[0], replace=False)  
 		gen = gen[idx] 
 
-	sigma2 = np.cov(gen, rowvar=False) 
-	mu2 = gen.mean(axis=0) 
-
+	sigma = np.cov(gen, rowvar=False) 
+	mu = gen.mean(axis=0) 
+	
+	# Training FID 
 	# calculate sum squared difference between means
-	ssdiff = np.sum((mu1 - mu2)**2.0)
+	ssdiffT = np.sum((muT - mu)**2.0)
 	# calculate sqrt of product between cov
-	covmean = linalg.sqrtm(sigma1.dot(sigma2))
+	covmeanT = linalg.sqrtm(sigmaT.dot(sigma))
 	# check and correct imaginary numbers from sqrt
-	if np.iscomplexobj(covmean):
-		covmean = covmean.real
+	if np.iscomplexobj(covmeanT):
+		covmeanT = covmeanT.real
 	# calculate score
-	fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
+	fidT = ssdiffT + np.trace(sigmaT + sigma - 2.0 * covmeanT)
 
-	print(fileName, fid) 
+	# Repeate for validation FID
+	ssdiffV = np.sum((muV - mu)**2.0)
+	covmeanV = linalg.sqrtm(sigmaV.dot(sigma))
+	if np.iscomplexobj(covmeanV):
+		covmeanV = covmeanV.real
+	fidV = ssdiffV + np.trace(sigmaV + sigma - 2.0 * covmeanV)
+
+	print(fileName, fidT, fidV) 
+	FIDs.append(fileName, fidT, fidV) 
+
+np.save("FID_values.npy", np.array(FIDs))  
 
 
-exit() 
+## GPU Code (WiP) -- CPU is sufficient for now 
 
-## GPU Code (WiP)  
 #acts = torch.from_numpy(np.load(fileName+"_FID.npy")) 
 #acts = acts.reshape(acts.size(0), -1) 
 
--- FID/plotFID.py
+++ FID/plotFID.py
@@ -1,32 +1,57 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
+# Load FID values as dictionary 
+FIDs = np.load("./FID_values.npy") 
+FIDs = {item[0]: item[1:].tolist() for item in FIDs} 
+
+
+# Configs
+outName = "FID_plot.png"
 epochs = [10,20,30,40,50,60,100,150,300]
 
+
+# Make sure all epochs in list have FIDs 
+tFIDs = [] 
+vFIDs = [] 
+for epoch in epochs: 
+	found = False 
+	for fileName, FID in FIDs.items(): 
+		if str(epoch) in fileName: 
+			found = True 
+			tFIDs.append(float(FID[0])) 
+			vFIDs.append(float(FID[1])) 
+			break
+	if not found: 
+		print("ERROR: FID missing for epoch", epoch) 
+		exit() 
+		 
+for i in range(len(epochs)): 
+	print(epochs[i], tFIDs[i], vFIDs[i])
+
 # Training Dataset (double_resnet[2])
-tFID = [116.430020195818, 12.2718659439913, 3.90431489082357,
-	3.06710681929949, 2.79309079926984, 2.74953105964787,
-	2.73551396671267, 2.6542792638002, 2.766824749261]
-tVQVAE = 42.0302090045512
+#tFIDs = [116.430020195818, 12.2718659439913, 3.90431489082357,
+#	3.06710681929949, 2.79309079926984, 2.74953105964787,
+#	2.73551396671267, 2.6542792638002, 2.766824749261]
+#tVQVAE = 42.0302090045512
 
 # Validation Dataset (double_resnet[2]) 
-vFID = [121.383403577486, 18.3063530684774, 10.2397028403388, 
-	9.205781860038, 9.09464218995247, 8.87166402235636, 
-	8.94381826410774, 8.70886043135631, 8.84371699914367]
-vVQVAE = 48.14133925
+#vFIDs = [121.383403577486, 18.3063530684774, 10.2397028403388, 
+#	9.205781860038, 9.09464218995247, 8.87166402235636, 
+#	8.94381826410774, 8.70886043135631, 8.84371699914367]
+#vVQVAE = 48.14133925
 
-# Train 10k Dataset 
-tFID = [121.625770012309, 18.9580374502103, 10.2323459642991,
-	9.1523467500183, 8.9416264096252, 8.99393980615258, 
-	8.95135264451978,8.74600565156756,8.78559773570689]
-vVQVAE10k = 47.54343442
-
+# Training 10k Dataset 
+#tFIDs = [121.625770012309, 18.9580374502103, 10.2323459642991,
+#	9.1523467500183, 8.9416264096252, 8.99393980615258, 
+#	8.95135264451978,8.74600565156756,8.78559773570689]
+#vVQVAE10k = 47.54343442
 
 plt.rcParams.update({'font.size': 16}) 
 
 plt.figure(figsize=(10,6)) 
-plt.plot(epochs, tFID, linestyle='-', marker='o', linewidth=3, markersize=10, label="Training Dataset (N=10k)") 
-plt.plot(epochs, vFID, linestyle='--', marker='o', linewidth=3, markersize=10, label="Validation Dataset (N=10k)") 
+plt.plot(epochs, tFIDs, linestyle='-', marker='o', linewidth=3, markersize=10, label="Training Dataset (N=10k)") 
+plt.plot(epochs, vFIDs, linestyle='--', marker='o', linewidth=3, markersize=10, label="Validation Dataset (N=10k)") 
 plt.yscale('log') 
 #plt.ylim(1, 15) 
 plt.ylim(7, 150) 
@@ -40,31 +65,31 @@ plt.xticks([10,30,50,100,150,200,250,300])
 
 if 0: 
 	# Annotate Epoch 20 
-	#plt.annotate("{:.2f}".format(tFID[1]), (epochs[1], tFID[1]), textcoords="offset points", xytext=(-25,0), ha='center') 
+	#plt.annotate("{:.2f}".format(tFIDss[1]), (epochs[1], tFIDss[1]), textcoords="offset points", xytext=(-25,0), ha='center') 
 
 	# Annotate Epoch 30 
-	plt.annotate("{:.2f}".format(tFID[2]), (epochs[2], tFID[2]), textcoords="offset points", xytext=(25,0), ha='center') 
-	#plt.annotate("{:.2f}".format(vFID[2]), (epochs[2], vFID[2]), textcoords="offset points", xytext=(30,0), ha='center') 
+	plt.annotate("{:.2f}".format(tFIDs[2]), (epochs[2], tFIDs[2]), textcoords="offset points", xytext=(25,0), ha='center') 
+	#plt.annotate("{:.2f}".format(vFIDs[2]), (epochs[2], vFIDs[2]), textcoords="offset points", xytext=(30,0), ha='center') 
 
 	# Annotate Epoch 40 
-	plt.annotate("{:.2f}".format(tFID[3]), (epochs[3], tFID[3]), textcoords="offset points", xytext=(10,10), ha='center') 
-	#plt.annotate("{:.2f}".format(vFID[3]), (epochs[3], vFID[3]), textcoords="offset points", xytext=(15,10), ha='center') 
+	plt.annotate("{:.2f}".format(tFIDs[3]), (epochs[3], tFIDs[3]), textcoords="offset points", xytext=(10,10), ha='center') 
+	#plt.annotate("{:.2f}".format(vFIDs[3]), (epochs[3], vFIDs[3]), textcoords="offset points", xytext=(15,10), ha='center') 
 	 
 	# Annotate Epoch 50 
-	plt.annotate("{:.2f}".format(tFID[4]), (epochs[4], tFID[4]), textcoords="offset points", xytext=(0,-20), ha='center') 
-	#plt.annotate("{:.2f}".format(vFID[4]), (epochs[4], vFID[4]), textcoords="offset points", xytext=(0,-20), ha='center') 
+	plt.annotate("{:.2f}".format(tFIDs[4]), (epochs[4], tFIDs[4]), textcoords="offset points", xytext=(0,-20), ha='center') 
+	#plt.annotate("{:.2f}".format(vFIDs[4]), (epochs[4], vFIDs[4]), textcoords="offset points", xytext=(0,-20), ha='center') 
 
 	# Annotate 60, 100, 150, 300 
 	for i in range(5, len(epochs)): 
-		annoT = "{:.2f}".format(tFID[i]) 
-		plt.annotate(annoT, (epochs[i], tFID[i]), textcoords="offset points", xytext=(0,10), ha='center') 
-		annoV = "{:.2f}".format(vFID[i]) 
-		#plt.annotate(annoV, (epochs[i], vFID[i]), textcoords="offset points", xytext=(0,10), ha='center') 
+		annoT = "{:.2f}".format(tFIDs[i]) 
+		plt.annotate(annoT, (epochs[i], [i]), textcoords="offset points", xytext=(0,10), ha='center') 
+		annoV = "{:.2f}".format(vFIDs[i]) 
+		#plt.annotate(annoV, (epochs[i], vFIDs[i]), textcoords="offset points", xytext=(0,10), ha='center') 
 
 plt.legend() 
 plt.tight_layout() 
 
-plt.savefig("FID_plot.png") 
+#plt.savefig(outName) 
 
 plt.show() 
 

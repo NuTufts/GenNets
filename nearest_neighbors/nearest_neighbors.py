f952ae50d906078aa52e3f8df57287dd74694f77
zevimani <Zeviel.Imani@tufts.edu>
2023-09-12 14:24:40 -0400

    Finalized Nearest Neighbors

--- nearest_neighbors/nearest_neighbors.py
+++ nearest_neighbors/nearest_neighbors.py
@@ -1,182 +1,144 @@
 #import matplotlib as mpl 
 #mpl.use('Agg') 
 import matplotlib.pyplot as plt
-from scipy.ndimage.filters import gaussian_filter
-from scipy.spatial import distance_matrix
+from absl import flags 
+from absl import app
 import itertools 
 import numpy as np
 import cv2 
-import ot
 
-# From Shaoib 
-from high_dim_tests import MaximumMeanDis_mix as MMD
-from sinkhorn_div import divergence as SinkDiv 
+set_eventNum = 0 
+set_eventType = "track" 
+set_distMode = 'l2'
+set_inDir = "/home/zimani/GenNets/npy_files/" 
+set_genFile = "gen_epoch50" 
+set_larFile = "larcv_png_64_train" 
 
+FLAGS = flags.FLAGS 
+flags.DEFINE_integer('eventNum', set_eventNum, 'Index of generated event to find neighbors for')
+flags.DEFINE_bool('select', False, 'Show 16 images, useful for choosing eventNum')
+flags.DEFINE_string('eventType', set_eventType, 'Track or shower ')
+flags.DEFINE_string('distMode', set_distMode, 'EMD or l2 for distance')
+flags.DEFINE_string('inDir', set_inDir, 'Directory location of numpy files')
+flags.DEFINE_bool('near', True, 'Nearest or farthest neighbors')
+flags.DEFINE_integer('numNeighbors', 5, 'Number of nearest neighbors to display')
+flags.DEFINE_string('genFile', set_genFile, 'Name of generated file without _tracks(showers)')
+flags.DEFINE_string('larFile', set_larFile, 'Name of comparison file without _tracks(showers)')
+flags.DEFINE_bool('showPlot', True, 'Show plot')
+flags.DEFINE_bool('saveNPY', False, 'Save distances as npy file')
 
-## Configurations 
+def main(argv): 
 
-inDir = "/home/zimani/GenNets/npy_files/" 
+	assert FLAGS.eventType in ["track", "shower"]
+	assert FLAGS.distMode in ["EMD", "l2"]
 
-# "track" or "shower"
-TorS = "track" 
+	gens = np.load(FLAGS.inDir+FLAGS.genFile+"_"+FLAGS.eventType+"s.npy") 
 
-genFile = "gen_epoch50" 
-gens = np.load(inDir+genFile+"_"+TorS+"s.npy") 
+	lars = np.load(FLAGS.inDir+FLAGS.larFile+"_"+FLAGS.eventType+"s.npy") 
 
-larFile = "larcv_png_64_train" 
-lars = np.load(inDir+larFile+"_"+TorS+"s.npy") 
-
-nNeighbors = 5 
-
-# "near" or "far" -est neighbors 
-nnDist = "near" 
-
-# l2 or EMD (or emperical distributions: MMD and SinkDiv)
-distMode = "EMD" 
-
-# Choose which generated image to use -- select by eye 
-imgNum = 178
-img = gens[imgNum,:] 
-if 0: 
-	fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(10, 10))
-	for i, ax in enumerate(axes.flatten()): 
-		ax.imshow(gens[imgNum+i,:], cmap='gray', interpolation='none')
-		ax.set_title(str(imgNum+i))
-		ax.axis('off') 
-	plt.tight_layout() 
-	plt.show() 
-	exit() 
-
-
-# Optional: apply small amount of gaussian noise to the image
-#gen = gaussian_filter(gen, sigma=1)
-
-# Hyperparameter for MMD 
-sigma_list = []
-for i in range(0,6): 
-	sigma_list.append(2**(i+2)) 
-
-# Hyperparameter for SinkDiv
-eps = 1 
-
-## End Configuration 
-
-if distMode == 'EMD':
-	# OpenCV EMD setup: signature matrix 
-	xx, yy = np.meshgrid(np.arange(64), np.arange(64))
-	xx = xx.ravel()
-	yy = yy.ravel()
-	img = img / np.sum(img) 
-	imgSig = np.vstack((img.ravel(), xx, yy)).T.astype(np.float32)
-
-	# POT EMD setup: L2 Distance matrix 
-	p = np.array(list(itertools.product(np.arange(64), repeat=2))) 
-	M = np.array(distance_matrix(p, p, p=2))
-
-if nnDist == 'near': 
-	nn = -1 
-else: 
-	nn = 1 
-
-# Initializations
-larDists = [] 
-genDists = [] 
-
-# Compare to training data 
-for lar in lars: 
-	if distMode == 'l2': 
-		larDist = int(np.linalg.norm(img - lar))
-	elif distMode == 'MMD': 
-		larDist = round(MMD(img, lar, sigma_list).item(),6) 
-	elif distMode == "SinkDiv": 
-		larDist = round(SinkDiv(img, lar, eps).item(), 2)
-	elif distMode == 'EMD': 
-		# OpenCV
-		lar = lar / np.sum(lar) 
-		larSig = np.vstack((lar.ravel(), xx, yy)).T.astype(np.float32)
-		larDist  = cv2.EMD(imgSig, larSig, cv2.DIST_L2)[0]
-		# POT - not work? 
-		#larDist = ot.emd2(img.ravel()/np.sum(img), lar.ravel()/np.sum(lar), M) 
-	else: 
-		print("Error: invalid distance mode") 
+	img = gens[FLAGS.eventNum,:] 
+	if FLAGS.select: 
+		fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(10, 10))
+		for i, ax in enumerate(axes.flatten()): 
+			ax.imshow(gens[FLAGS.eventNum+i,:], cmap='gray', interpolation='none')
+			ax.set_title(str(FLAGS.eventNum+i))
+			ax.axis('off') 
+		plt.tight_layout() 
+		plt.show() 
 		exit() 
 
-	larDists.append(larDist) 
+	if FLAGS.distMode == 'EMD':
+		# OpenCV EMD setup: signature matrix 
+		xx, yy = np.meshgrid(np.arange(64), np.arange(64))
+		xx = xx.ravel()
+		yy = yy.ravel()
+		img = img / np.sum(img) 
+		imgSig = np.vstack((img.ravel(), xx, yy)).T.astype(np.float32)
 
-larDists = np.asarray(larDists)
-if distMode == 'EMD': 
-	np.save("./emd_npy/emd_dists_lartpc_"+TorS+"_"+str(imgNum)+".npy", larDists) 
-if distMode == 'l2':  
-	np.save("./l2_npy/l2_dists_lartpc_"+TorS+"_"+str(imgNum)+".npy", genDists) 
+	if FLAGS.near: 
+		nn = -1 
+	else: 
+		nn = 1 
 
-# Sort nearest neighbors 
-idxSortL = (nn*larDists).argsort() 
-larDists = larDists[idxSortL[::-1]] 
-larSort = lars[idxSortL[::-1]] 
+	# Initializations
+	larDists = [] 
+	genDists = [] 
 
-# Compare to generated data
-for gen in gens: 
-	if distMode == 'l2': 
-		genDist = int(np.linalg.norm(img - gen))
-	if distMode == 'MMD': 
-		genDist = round(MMD(img, gen, sigma_list).item(), 6)
-	if distMode == "SinkDiv": 
-		genDist = round(SinkDiv(img, gen, eps).item(), 2)
-	if distMode == 'EMD': 
-		# OpenCv
-		gen = gen / np.sum(gen) 
-		genSig = np.vstack((gen.ravel(), xx, yy)).T.astype(np.float32)
-		genDist = cv2.EMD(imgSig, genSig, cv2.DIST_L2)[0]
-		# POT - not work? 
-		#genDist = ot.emd2(img.ravel()/np.sum(img), gen.ravel()/np.sum(gen), M) 
+	# Compare to training data 
+	for lar in lars: 
+		if FLAGS.distMode == 'l2': 
+			larDist = int(np.linalg.norm(img - lar))
+		elif FLAGS.distMode == 'EMD': 
+			lar = lar / np.sum(lar) 
+			larSig = np.vstack((lar.ravel(), xx, yy)).T.astype(np.float32)
+			larDist  = cv2.EMD(imgSig, larSig, cv2.DIST_L2)[0]
+		else: 
+			print("Error: invalid distMode") 
+			exit() 
+		larDists.append(larDist) 
+	larDists = np.asarray(larDists)
+	print("Training Neighbors Found")
 
-	genDists.append(genDist) 
+	if FLAGS.saveNPY: 
+		np.save(FLAGS.distMode+"_dists_lar_"+FLAGS.eventType+"_"+str(FLAGS.eventNum)+".npy", larDists) 
 
-genDists = np.asarray(genDists) 
-if distMode == 'EMD': 
-	np.save("./emd_npy/emd_dists_gen_"+TorS+"_"+str(imgNum)+".npy", genDists) 
-if distMode == 'l2':  
-	np.save("./l2_npy/l2_dists_gen_"+TorS+"_"+str(imgNum)+".npy", genDists) 
+	# Sort nearest neighbors 
+	idxSortL = (nn*larDists).argsort() 
+	larDists = larDists[idxSortL[::-1]] 
+	larSort = lars[idxSortL[::-1]] 
 
-# Sort nearest neighbors 
-idxSortG = (nn*genDists).argsort() 
-genDists = genDists[idxSortG[::-1]] 
-genSort = gens[idxSortG[::-1]] 
+	# Compare to generated data
+	for gen in gens: 
+		if FLAGS.distMode == 'l2': 
+			genDist = int(np.linalg.norm(img - gen))
+		elif FLAGS.distMode == 'EMD': 
+			gen = gen / np.sum(gen) 
+			genSig = np.vstack((gen.ravel(), xx, yy)).T.astype(np.float32)
+			genDist = cv2.EMD(imgSig, genSig, cv2.DIST_L2)[0]
+		genDists.append(genDist) 
+	genDists = np.asarray(genDists) 
+	print("Generated Neighbors Found")
 
-# Remove self match - need adjust nNeighbors
-if nnDist == 'near': 
-	genDists = genDists[1:] 
-	genSort = genSort[1:] 
+	if FLAGS.saveNPY: 
+		np.save(FLAGS.distMode+"_dists_gen_"+FLAGS.eventType+"_"+str(FLAGS.eventNum)+".npy", genDists) 
 
-#print("Done") 
-#exit() 
+	# Sort nearest neighbors 
+	idxSortG = (nn*genDists).argsort() 
+	genDists = genDists[idxSortG[::-1]] 
+	genSort = gens[idxSortG[::-1]] 
 
-## Plotting 
+	# Remove self match
+	if FLAGS.near: 
+		genDists = genDists[1:] 
+		genSort = genSort[1:] 
 
-plt.figure(figsize=(14,4)) 
+	## Plotting 
 
-plt.subplot2grid(shape=(2, 7), loc=(0,0), colspan=2, rowspan=2) 
-plt.imshow(img, cmap='gray', interpolation='nearest') 
-plt.title("Generated") 
-plt.axis('off') 
+	plt.figure(figsize=(14,4)) 
 
-for i in range(nNeighbors): 
-	plt.subplot2grid((2,7), (0,i+2)) 
-	plt.imshow(larSort[i], cmap='gray', interpolation='nearest')
-	plt.title(larDists[i]) 
+	plt.subplot2grid(shape=(2, 7), loc=(0,0), colspan=2, rowspan=2) 
+	plt.imshow(img, cmap='gray', interpolation='nearest') 
+	plt.title("Generated") 
 	plt.axis('off') 
 
-	plt.subplot2grid((2,7), (1,i+2)) 
-	plt.imshow(genSort[i], cmap='gray', interpolation='nearest')
-	plt.title(genDists[i]) 
-	plt.axis('off') 
+	for i in range(FLAGS.numNeighbors): 
+		plt.subplot2grid((2,7), (0,i+2)) 
+		plt.imshow(larSort[i], cmap='gray', interpolation='nearest')
+		plt.title(larDists[i]) 
+		plt.axis('off') 
 
-plt.tight_layout() 
+		plt.subplot2grid((2,7), (1,i+2)) 
+		plt.imshow(genSort[i], cmap='gray', interpolation='nearest')
+		plt.title(genDists[i]) 
+		plt.axis('off') 
 
-plt.savefig("./emd_plots/"+distMode+"_neighbors_"+TorS+"_"+str(imgNum)+".png")
-#plt.savefig("./l2_plots/"+distMode+"_neighbors_"+TorS+"_"+str(imgNum)+".png")
-#plt.savefig("./"+distMode+"_neighbors_"+TorS+"_"+str(imgNum)+".png")
-print("Saved Fig") 
+	plt.tight_layout() 
 
-plt.show() 
+	plt.savefig(FLAGS.distMode+"_neighbors_"+FLAGS.eventType+"_"+str(FLAGS.eventNum)+".png")
+	print("Saved", FLAGS.distMode+"_neighbors_"+FLAGS.eventType+"_"+str(FLAGS.eventNum)+".png") 
 
+	if FLAGS.showPlot: 
+		plt.show() 
+
+if __name__ == '__main__': 
+	app.run(main) 
